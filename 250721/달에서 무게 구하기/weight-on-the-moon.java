d4ed6fc8a3e0f7ec8dfd7c14774fbc85fe983547
Whylano <112239843+Whylano@users.noreply.github.com>
2025-07-21 22:13:38 +0900

    Title: 달에서 무게 구하기, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250721/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a;
+        double b;
+        a = 13;
+        b = 0.165;
+        System.out.print(a + " * ");
+        System.out.printf("%.6f = ",b);
+        System.out.printf("%.6f",((double)a)*b);
+    }
+}
