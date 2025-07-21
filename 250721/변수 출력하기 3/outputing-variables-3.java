5d41ef48405765644e571c6f3ae99c98574fa0a5
Whylano <112239843+Whylano@users.noreply.github.com>
2025-07-21 21:59:25 +0900

    Title: 변수 출력하기 3, Time: 88ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250721/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a,b;
+        char c;
+        a = 1;
+        b = 2;
+        c = 'C';
+        System.out.println(a+"->"+b+"->"+c);
+    }
+}
