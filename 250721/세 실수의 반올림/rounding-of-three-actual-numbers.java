af9426cce3529d1f1b7e95e2c5a54efe1e4a3e8f
Whylano <112239843+Whylano@users.noreply.github.com>
2025-07-21 22:43:08 +0900

    Title: 세 실수의 반올림, Time: 149ms, Memory: 11MB, Status: Passed - Codetree

--- 250721/세 실수의 반올림/rounding-of-three-actual-numbers.java
+++ 250721/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -12,10 +12,8 @@ public class Main {
         a = Math.round(a * 1000) / 1000.0; 
         b = Math.round(b * 1000) / 1000.0; 
         c = Math.round(c * 1000) / 1000.0; 
-        String result1 = String.format("%.3f", a);
-        String result2 = String.format("%.3f", b);
-        String result3 = String.format("%.3f", c);
-        DecimalFormat decimalFormat = new DecimalFormat("###.000");
-        System.out.println(a+"\n"+b+"\n"+decimalFormat.format(c));
+        System.out.printf("%.3f\n" ,a);
+        System.out.printf("%.3f\n" ,b);
+        System.out.printf("%.3f" ,c);
     }
 }
