22ac756760328e436b53804f3bb4ee1884177b43
Whylano <112239843+Whylano@users.noreply.github.com>
2025-07-21 21:56:43 +0900

    Title: 사칙연산 연습, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250721/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a,b,c;
+        a = 5;
+        b = 30;
+        c = a+b;
+        System.out.println(c/8);
+        System.out.println(c%8);
+
+    }
+}
