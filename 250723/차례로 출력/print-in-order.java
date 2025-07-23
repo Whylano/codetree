e36c58f12734a40262e6a79a4cf263bc2f46b20b
Whylano <112239843+Whylano@users.noreply.github.com>
2025-07-23 23:23:07 +0900

    Title: 차례로 출력, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250723/차례로 출력/print-in-order.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder res1 = new StringBuilder();
+        for (int i = 5; i <= 17; i++) {
+            res1.append(i+" ");
+        }
+        System.out.println(res1);
+    }
+}
