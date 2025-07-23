c103469b7105b2fca0667e0c197aeb848f8cf4e1
Whylano <112239843+Whylano@users.noreply.github.com>
2025-07-23 23:08:21 +0900

    Title: 등급 매기기, Time: 111ms, Memory: 10MB, Status: Passed - Codetree

--- 250723/등급 매기기/ranking.java
+++ 250723/등급 매기기/ranking.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if(a>90){
+        if(a>=90){
             System.out.println("A");
         }else if(a>=80){
             System.out.println("B");
